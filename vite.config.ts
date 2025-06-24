96f3d7e9d49390f2aa8c5535582363776565429d
4+ <h660@foxmail.com>
2025-06-24 17:07:59 +0800

    25-06-24.2

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'docs', // 将默认的 "dist" 改为 "docs"
+  },
 })
