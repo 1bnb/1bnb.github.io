1c8dac58a676e010e85e84f9eea7927101419511
4+ <h660@foxmail.com>
2025-08-17 15:57:22 +0800

    25.08.17

--- src/views/Case.vue
+++ src/views/Case.vue
@@ -16,6 +16,16 @@ import pumpfun_1 from "../assets/pumpfun_1.png"
 import pumpfun_2 from "../assets/pumpfun_2.png"
 import smart_1 from "../assets/smart_1.png"
 import smart_2 from "../assets/smart_2.png"
+import tg_1 from "../assets/tg_1.png"
+import tg_2 from "../assets/tg_2.png"
+import tg_3 from "../assets/tg_3.png"
+import tg_4 from "../assets/tg_4.png"
+import tg_5 from "../assets/tg_5.png"
+import tg_6 from "../assets/tg_6.png"
+import tg_7 from "../assets/tg_7.png"
+import tg_8 from "../assets/tg_8.png"
+import tg_9 from "../assets/tg_9.png"
+import tg_10 from "../assets/tg_10.png"
 
 const data = [{
     title: "Blockchain Defi Project",
@@ -71,6 +81,20 @@ const data = [{
     ]
 }]
 
+const apps = [{
+    title: "Tg MiniApp Defi",
+    content: "A decentralized application that lets users earn rewards simply by utilizing their products, creating value from everyday usage through blockchain technology.",
+    photo: [
+        { id: 1, name: "1", photo: tg_1 },
+        { id: 2, name: "2", photo: tg_2 },
+        { id: 3, name: "3", photo: tg_3 },
+        { id: 4, name: "4", photo: tg_4 },
+        { id: 5, name: "5", photo: tg_5 },
+        { id: 6, name: "6", photo: tg_6 },
+        { id: 7, name: "7", photo: tg_7 },
+        { id: 8, name: "8", photo: tg_8 },
+    ]
+}]
 
 </script>
 
@@ -84,6 +108,22 @@ const data = [{
             </div>
             <div class="flex flex-wrap -mx-4 -mb-10 text-center">
                 <div v-for="item in items.photo" :key="item.id" class="sm:w-1/2 mb-10 px-4">
+                    <div class="rounded-lg overflow-hidden">
+                        <img alt="content" class="object-cover object-center h-full w-full" :src=item.photo>
+                    </div>
+                    <h2 class="title-font text-2xl font-medium text-gray-300 mt-6 mb-3 capitalize">{{ item.name }}</h2>
+                </div>
+            </div>
+        </div>
+        <!-- dapp案例 -->
+        <div v-for="(items, index) in apps" :key="index" class="container lg:px-20 px-5 pt-24 mx-auto">
+            <div class="title_ w-full mb-5">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-300 capitalize">{{ items.title }}
+                </h1>
+                <p class="w-full leading-relaxed text-gray-500 text-left">{{ items.content }}</p>
+            </div>
+            <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+                <div v-for="item in items.photo" :key="item.id" class="sm:w-1/4 mb-10 px-4">
                     <div class="rounded-lg overflow-hidden">
                         <img alt="content" class="object-cover object-center h-full w-full" :src=item.photo>
                     </div>
